94b6112f154fba9a40dc4218c6c2c97983d887b8
JeremiahAmir <jeremiah@creative-dots.com>
2024-04-21 18:32:08 +0500

    update routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,25 +1,21 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Feeds from "../pages/feeds/Feeds";
 import News from "../pages/news/News";
 
 export const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
         path: "/",
-        element: <Main />,
-        children: [
-            {
-                path: "/",
-                element: <Feeds />
-            },
-            {
-                path: "/feeds",
-                element: <Feeds />,
-            },
-            {
-                path: "/news",
-                element: <News />,
-            },
-        ],
-    },
+        element: <Feeds />,
+      },
+      {
+        path: "/news",
+        element: <News />,
+      },
+    ],
+  },
 ]);
