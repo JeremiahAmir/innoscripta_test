8d247132c3d0c82ccf12ca85724f2cfcc4285e37
JeremiahAmir <jeremiah@creative-dots.com>
2024-04-21 19:13:53 +0500

    Updated the styles

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,21 @@ import "./index.css";
 import { Router } from "./router/router";
 
 const steps = [
-  {
-    selector: "#sidebarCollapse",
-    content: "You can update your feed preferences here.",
-  },
+    {
+        selector: "#sidebarCollapse",
+        content: "You can update your feed preferences here.",
+    },
 ];
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
 root.render(
-  <TourProvider
-    steps={steps}
-    defaultOpen={!localStorage.getItem("tour")}
-    afterOpen={() => localStorage.setItem("tour", "true")}
-  >
-    <RouterProvider router={Router} />
-  </TourProvider>
+    <TourProvider
+        steps={steps}
+        defaultOpen={!localStorage.getItem("tour")}
+        afterOpen={() => localStorage.setItem("tour", "true")}
+    >
+        <RouterProvider router={Router} />
+    </TourProvider>
 );
